bab397ae5e997f7d451374e8ed0e51cb11186a35
Khanh Trinh <xuaankhanhs@gmail.com>
2022-05-16 23:10:53 +0700

    Config MongoDB

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,12 +1,31 @@
-const dotenv = require('dotenv');
-const path = require('path');
-const Joi = require('joi');
-dotenv.config({ path: path.join(__dirname, '../../.env') });
+const dotenv = require("dotenv");
+const path = require("path");
+const Joi = require("joi");
+
+dotenv.config({ path: path.join(__dirname, "../../.env") });
 
 const envVariablesSchema = Joi.object().keys({
-  PORT: Joi.number().default(3000)
-})
+  PORT: Joi.number().default(3000),
+  USERNAME_MONGO: Joi.string().required(),
+  PASSWORD_MONGO: Joi.string().required(),
+  MONGODB_URL: Joi.string().required().description("Mongo DB url"),
+});
 
-const { value: { PORT } } = envVariablesSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+const {
+  value: { PORT, USERNAME_MONGO, PASSWORD_MONGO, MONGODB_URL },
+} = envVariablesSchema
+  .prefs({ errors: { label: "key" } })
+  .validate(process.env);
 
-module.exports = { PORT };
+const CERTIFICATE = {
+  MONGODB_URL: MONGODB_URL.replace("<username>", USERNAME_MONGO).replace(
+    "<password>",
+    PASSWORD_MONGO
+  ),
+  OPTIONS: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+};
+
+module.exports = { PORT, CERTIFICATE };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-const { log } = require('console');
-const app = require('./app');
-const { PORT } = require('./config/environment')
+const { log } = require("console");
+const app = require("./app");
+const { PORT, CERTIFICATE } = require("./config/environment");
+const mongoose = require("mongoose");
 
-app.listen(PORT, () => {
-  log(`Listening to port ${PORT}`);
+mongoose.connect(CERTIFICATE.MONGODB_URL, CERTIFICATE.OPTIONS).then(() => {
+  log("Connected to MongoDB");
+
+  app.listen(PORT, () => {
+    log(`Listening to port ${PORT}`);
+  });
 });
